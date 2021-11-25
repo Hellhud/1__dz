ddac3bc2c4371f3a2c5665a47aa6413bea95542f
Hellhound <hardoneloves@gmail.com>
2021-11-25 20:44:43 +0300

    Добавьте файлы проекта.

---
+++ 1__dz/1__dz.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+bool ty(double a[], int s,int t=0) {
+	for (size_t i = 0; i < s; i++) {
+		a[i] = rand() % 100;
+		std::cout << a[i] << " ";
+	}
+	std::cout << std::endl;
+	return true;
+}
+int main(){
+	int s = 10;
+	double a[10];
+	ty(a, s);
+	
+
+}
